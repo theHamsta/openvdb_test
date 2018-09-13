1ec0629d969c96aa659dc037edec86401927dfcc
Stephan Seitz <stephan.seitz@fau.de>
2018-09-13 14:48:49 +0200

    Change example to output .vdb file

--- main.cpp
+++ main.cpp
@@ -1,36 +1,24 @@
 #include <openvdb/openvdb.h>
-#include <iostream>
+#include <openvdb/tools/LevelSetSphere.h>
 int main()
 {
-    // Initialize the OpenVDB library.  This must be called at least
-    // once per program and may safely be called multiple times.
     openvdb::initialize();
-    // Create an empty floating-point grid with background value 0.
-    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
-    std::cout << "Testing random access:" << std::endl;
-    // Get an accessor for coordinate-based access to voxels.
-    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
-    // Define a coordinate with large signed indices.
-    openvdb::Coord xyz(1000, -200000000, 30000000);
-    // Set the voxel value at (1000, -200000000, 30000000) to 1.
-    accessor.setValue(xyz, 1.0);
-    // Verify that the voxel value at (1000, -200000000, 30000000) is 1.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
-    // Reset the coordinates to those of a different voxel.
-    xyz.reset(1000, 200000000, -30000000);
-    // Verify that the voxel value at (1000, 200000000, -30000000) is
-    // the background value, 0.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
-    // Set the voxel value at (1000, 200000000, -30000000) to 2.
-    accessor.setValue(xyz, 2.0);
-    // Set the voxels at the two extremes of the available coordinate space.
-    // For 32-bit signed coordinates these are (-2147483648, -2147483648, -2147483648)
-    // and (2147483647, 2147483647, 2147483647).
-    accessor.setValue(openvdb::Coord::min(), 3.0f);
-    accessor.setValue(openvdb::Coord::max(), 4.0f);
-    std::cout << "Testing sequential access:" << std::endl;
-    // Print all active ("on") voxels by means of an iterator.
-    for (openvdb::FloatGrid::ValueOnCIter iter = grid->cbeginValueOn(); iter; ++iter) {
-        std::cout << "Grid" << iter.getCoord() << " = " << *iter << std::endl;
-    }
+    // Create a FloatGrid and populate it with a narrow-band
+    // signed distance field of a sphere.
+    openvdb::FloatGrid::Ptr grid =
+        openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(
+            /*radius=*/50.0, /*center=*/openvdb::Vec3f(1.5, 2, 3),
+            /*voxel size=*/0.5, /*width=*/4.0);
+    // Associate some metadata with the grid.
+    grid->insertMeta("radius", openvdb::FloatMetadata(50.0));
+    // Name the grid "LevelSetSphere".
+    grid->setName("LevelSetSphere");
+    // Create a VDB file object.
+    openvdb::io::File file("mygrids.vdb");
+    // Add the grid pointer to a container.
+    openvdb::GridPtrVec grids;
+    grids.push_back(grid);
+    // Write out the contents of the container.
+    file.write(grids);
+    file.close();
 }
